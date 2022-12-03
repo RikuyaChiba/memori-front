fb36106624e97cd367a2c7273fd3c6cf9a2f16cf
Rikuya Chiba <rikuya.chiba@gmail.com>
2022-12-03 21:39:46 +0900

    Add file for constants

---
+++ src/config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'recurring' => [
+        'status' => [
+            'first_day' => 1,
+            'one_day_recurring' => 2,
+            'three_day_recurring' => 3,
+            'one_week_recurring' => 4,
+            'two_week_recurring' => 5,
+            'one_month_recurring' => 6
+        ]
+    ]
+];
